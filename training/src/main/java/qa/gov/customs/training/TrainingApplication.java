d8e76ffc724dd739a49515c18a23983577b6456f
sarath raj <sraj@customs.gov.qa>
2019-04-10 10:55:00 +0300

    training project updated with yml

---
+++ training/src/main/java/qa/gov/customs/training/TrainingApplication.java
@@ -0,0 +1,14 @@
+package qa.gov.customs.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingApplication.class, args);
+    }
+
+}
