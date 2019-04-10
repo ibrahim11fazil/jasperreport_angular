9e25bfb2051d051042365ac7cb82a78f14f3af4c
sarath raj <sraj@customs.gov.qa>
2019-04-10 08:30:58 +0300

    added main class

---
+++ maward/src/main/java/qa/gov/customs/maward/MawardApplication.java
@@ -0,0 +1,14 @@
+package qa.gov.customs.maward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MawardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MawardApplication.class, args);
+    }
+
+}
