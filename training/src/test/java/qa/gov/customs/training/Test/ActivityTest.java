5670eab39c0de34f4dc740a662514ed092cf42d8
jayasree <jayasree@customs.gov.qa>
2019-04-11 09:13:03 +0300

    updated application.yml

--- training/src/test/java/qa/gov/customs/training/Test/ActivityTest.java
+++ training/src/test/java/qa/gov/customs/training/Test/ActivityTest.java
@@ -1,5 +1,31 @@
 package qa.gov.customs.training.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import qa.gov.customs.training.entity.TacActivity;
+import qa.gov.customs.training.repository.ActivityRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
 public class ActivityTest {
 
+	
+	   @Autowired
+	   ActivityRepository activityRepository;
+
+	   @Test
+	   public void jpaTestGetAllActivity()
+	   {
+		 ArrayList< TacActivity> activity=(ArrayList<TacActivity>) activityRepository.findAll();
+		   assertThat(activity!=null).isEqualTo(true);
+	    }
+	   
 }
