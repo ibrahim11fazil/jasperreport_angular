04a5535d36f38e766b0562e5766382c2d0f56dd0
sarath raj <sraj@customs.gov.qa>
2019-04-24 12:06:18 +0300

    testing data

--- notification/src/main/java/qa/gov/customs/notification/security_config/CustomPrincipal.java
+++ notification/src/main/java/qa/gov/customs/notification/security_config/CustomPrincipal.java
@@ -1,11 +1,14 @@
 package qa.gov.customs.notification.security_config;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 
 public class CustomPrincipal implements Serializable {
     private static final long serialVersionUID = 1L;
     private String username;
     private String email;
+    private BigInteger enabled;
+    private BigInteger credentialsExpired;
 
     public CustomPrincipal() {
     }
@@ -13,6 +16,21 @@ public class CustomPrincipal implements Serializable {
     public CustomPrincipal(String username, String email) {
         this.username = username;
         this.email = email;
+    }
+
+    public CustomPrincipal(String username, String email, BigInteger enabled, BigInteger credentialsExpired) {
+        this.username = username;
+        this.email = email;
+        this.enabled = enabled;
+        this.credentialsExpired = credentialsExpired;
+    }
+
+    public BigInteger getCredentialsExpired() {
+        return credentialsExpired;
+    }
+
+    public void setCredentialsExpired(BigInteger credentialsExpired) {
+        this.credentialsExpired = credentialsExpired;
     }
 
     public String getUsername() {
@@ -29,5 +47,13 @@ public class CustomPrincipal implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public BigInteger getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(BigInteger enabled) {
+        this.enabled = enabled;
     }
 }
