31564162e6fe9663c4350e88dd0b3a6401f37b6e
sarath raj <sraj@customs.gov.qa>
2019-04-24 07:23:56 +0300

    updated utils

---
+++ utils/src/main/java/qa/gov/customs/utils/models/ResponseType.java
@@ -0,0 +1,52 @@
+package qa.gov.customs.utils.models;
+
+public class ResponseType {
+ private int code;
+ private String message;
+ private boolean status;
+ private int count;
+ private Object data;
+ 
+ 
+ 
+ 
+public ResponseType(int code, String message, boolean status, Object data) {
+	super();
+	this.code = code;
+	this.message = message;
+	this.status = status;
+	this.data = data;
+}
+public ResponseType() {
+}
+public int getCode() {
+	return code;
+}
+public void setCode(int code) {
+	this.code = code;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public boolean isStatus() {
+	return status;
+}
+public void setStatus(boolean status) {
+	this.status = status;
+}
+public Object getData() {
+	return data;
+}
+public void setData(Object data) {
+	this.data = data;
+}
+public int getCount() {
+	return count;
+}
+public void setCount(int count) {
+	this.count = count;
+}
+}
