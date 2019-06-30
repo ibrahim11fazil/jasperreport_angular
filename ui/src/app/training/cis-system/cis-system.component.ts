147abbf73e32079998d8c1d049a33c99b76f9235
sarath raj <sraj@customs.gov.qa>
2019-06-30 08:37:19 +0300

    ci system user can request from tac

--- ui/src/app/training/cis-system/cis-system.component.ts
+++ ui/src/app/training/cis-system/cis-system.component.ts
@@ -20,7 +20,7 @@ export class CisSystemComponent implements OnInit {
   page = 0
   ds: CiSystemUser[] = [];
   firstSearch=false
-  displayedColumns: string[] = ['id', 'jobCode', 'fullName', 'deparatment', 'decision','decisionDetails','decisionDate'];
+  displayedColumns: string[] = ['id', 'jobCode', 'fullName', 'deparatment', 'decision','decisionDetails','decisionDate','Request'];
   constructor(
     private trainingService: TrainingService,
     private fb: FormBuilder,
