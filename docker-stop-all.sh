cc234f6fc8d580175741009b5f48e3eaab0695e7
sarath raj <sraj@customs.gov.qa>
2019-07-02 06:35:00 +0300

    upated stopping and removing containers

--- docker-stop-all.sh
+++ docker-stop-all.sh
@@ -1,8 +1,25 @@
 
-docker stop $(docker ps -a -q --filter 'name=^/training')
+DOCKER_CONTAINER_IDS=$(docker ps -a -q --filter 'name=^/training')
 
-docker rm $(docker ps -a -q --filter 'name=^/training')
+if [ -z "$DOCKER_CONTAINER_IDS" -o "$DOCKER_CONTAINER_IDS" == " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+     docker stop  $DOCKER_CONTAINER_IDS
+     docker rm  $DOCKER_CONTAINER_IDS
+fi
 
-docker rmi $(docker images |grep 'training')
+DOCKER_IMAGE_IDS=$(docker images |grep 'training')
+
+if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+     docker rmi  $DOCKER_IMAGE_IDS
+fi
+
+#docker stop `docker ps -a -q --filter 'name=^/training'`
+
+#docker rm  `docker ps -a -q --filter 'name=^/training'`
+
+#docker rmi `docker images |grep 'training'`
 
 echo 'finished cleaning'
