1e6dd0ac8962047632e133eec96256b2a53eb036
sarath raj <sraj@customs.gov.qa>
2019-07-05 21:03:55 +0300

    build version 0.0.5

--- gittag.sh
+++ gittag.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
  echo "Current version is "   $(git describe --abbrev=0)
-if [ "$1" != "" ]
+if [ "$1" != "" ] && [ "$2" != "" ]
     then
      mvn versions:set -DnewVersion=$1
+     git add -A
+     git commit -m "build version $1"
+     git push origin $2
      git tag -a $1 -m "version $1"
      git push --tags
      mvn versions:commit
      echo "Version updated to "   $(git describe --abbrev=0)
     else
      echo "Enter the version"
+     echo "Enter the git branch to push to server , use current and master"
      echo "Current version is "   $(git describe --abbrev=0)
 fi
