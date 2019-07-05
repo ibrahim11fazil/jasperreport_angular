793702e77dada809897ebaafb47f3da1e81acc29
sarath raj <sraj@customs.gov.qa>
2019-07-05 15:54:50 +0300

    updated docker file for volumes

--- docker-pull-qa.sh
+++ docker-pull-qa.sh
@@ -3,18 +3,19 @@ set -a
 source env_qa.txt
 set +a
 
-docker network create --driver=bridge --subnet=172.16.7.0/16 --gateway=172.16.202.202 networktraining
+# this will create network issues when communicating in server
+#docker network create --driver=bridge --subnet=172.16.7.0/16 --gateway=172.16.202.202 networktraining
 
 TAG=$1
-docker run -d --env-file=./env_qa.txt  -p 9071:9071 --ip=$TRAINING_UI_IP  --network=networktraining  172.16.0.253:5000/ui:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9061:9061 --ip=WORKFLOW_IP --network=networktraining  172.16.0.253:5000/workflow:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9041:9041 --ip=NOTIFICATION_IP --network=networktraining  172.16.0.253:5000/notification:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9031:9031 --ip=USER_IP --network=networktraining  172.16.0.253:5000/user:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9011:9011 --ip=EMPLOYEE_IP --network=networktraining  172.16.0.253:5000/employee:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9071:9071 --ip=CIS_INTEGRATION_IP --network=networktraining  172.16.0.253:5000/cis:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9051:9051 --ip=TRAINING_IP --network=networktraining  172.16.0.253:5000/training:$TAG
-docker run -d --env-file=./env_qa.txt  -p 8888:8888 --ip=CONFIG_IP --network=networktraining  172.16.0.253:5000/config:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9021:9021 --ip=FILE_UPLOAD_IP --network=networktraining  172.16.0.253:5000/fileupload:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9001:9001 --ip=OAUTH_IP --network=networktraining  172.16.0.253:5000/authentication:$TAG
-docker run -d --env-file=./env_qa.txt  -p 8761:8761 --ip=REGISTRY_IP --network=networktraining  172.16.0.253:5000/registry:$TAG
-docker run -d --env-file=./env_qa.txt  -p 9000:9000 --ip=GATEWAY_IP --network=networktraining  172.16.0.253:5000/gateway:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 80:80      172.16.0.253:5000/ui:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9061:9061   172.16.0.253:5000/workflow:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9041:9041   172.16.0.253:5000/notification:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9031:9031   172.16.0.253:5000/user:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9011:9011   172.16.0.253:5000/employee:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9071:9071   172.16.0.253:5000/cis:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9051:9051   172.16.0.253:5000/training:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 8888:8888   172.16.0.253:5000/config:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -v  $UPLOAD_PATH:/uploads -p 9021:9021   172.16.0.253:5000/fileupload:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9001:9001  172.16.0.253:5000/authentication:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 8761:8761  172.16.0.253:5000/registry:$TAG
+docker run -d --env-file=./env_qa.txt -v $LOG_PATH:/logs -v  $TMP_PATH:/tmp -p 9000:9000  172.16.0.253:5000/gateway:$TAG
--- notification/src/main/java/qa/gov/customs/notification/NotificationApplication.java
+++ notification/src/main/java/qa/gov/customs/notification/NotificationApplication.java
@@ -2,14 +2,14 @@ package qa.gov.customs.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-//@ComponentScan(basePackages = "qa.gov.customs.notification.security_config")
-//@ComponentScan(basePackages = {"qa.gov.customs.utils.config"})
+@EnableDiscoveryClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
