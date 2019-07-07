ea9750349312ac3ffc33e159aea5b54471e8058d
sarath raj <sraj@customs.gov.qa>
2019-07-07 08:59:53 +0300

    build version 0.1.4

--- gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
+++ gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
@@ -1,14 +1,26 @@
 package qa.gov.customs.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.Optional;
 
 
 //@EnableResourceServer
@@ -30,6 +42,8 @@ public class GatewayApplication {
         return new Filter();
     }
 
+    private EurekaInstanceConfigBean eurekaInstanceConfig;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
     public CorsFilter corsFilter() {
@@ -47,5 +61,70 @@ public class GatewayApplication {
         config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+
+    @Primary
+    @Autowired
+//    @Profile("docker")
+    public EurekaInstanceConfigBean DockerSwarm_EurekaClient(InetUtils inetUtils)
+    {
+
+
+        try{
+            eurekaInstanceConfig= new EurekaInstanceConfigBean(inetUtils);
+
+            final String HostName = System.getenv("HOSTNAME"); //container hostname 가져옴, container_id 일 것임.
+            logger.info("HOSTNAME : " + HostName);
+
+
+            Optional<NetworkInterface> net = Optional.of(NetworkInterface.getByName("eth2"));
+
+            logger.info("Network instance inetaddress: " + net.get().getInetAddresses());
+            logger.info("Network instance name: " + net.get().getName());
+
+            Enumeration<InetAddress> inetAddress = net.get().getInetAddresses();
+
+            InetAddress current = inetAddress.nextElement();
+
+            logger.info("Get Current Address : " + current.toString());
+
+            String address = current.toString().split("/")[1];
+
+/*
+			String address = null;
+
+			while(inetAddress.hasMoreElements())
+			{
+				current = inetAddress.nextElement();
+                logger.info("current address 1 : " + current.toString());
+				if(!current.isLoopbackAddress())
+				{
+					address = current.toString();
+                    logger.info("current address2: ", address);
+					break;
+				}
+			}
+*/
+            logger.info(" HostName : " + HostName);
+            logger.info(" Address : " + address);
+
+            eurekaInstanceConfig.setHostname(HostName);
+            eurekaInstanceConfig.setPreferIpAddress(true);
+            eurekaInstanceConfig.setIpAddress(address);
+            eurekaInstanceConfig.setNonSecurePort(9000);
+
+            logger.info("Eureka Config : "  + eurekaInstanceConfig.toString());
+
+            return eurekaInstanceConfig;
+
+
+        }catch(Exception e)
+        {
+            logger.info("EEEEEEEEEEEEEEERRRR");
+            return null;
+        }
+
+
     }
 }
