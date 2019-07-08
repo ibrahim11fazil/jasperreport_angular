0b1cef3464c393d510ca0167352e074f50718297
sarath raj <sraj@customs.gov.qa>
2019-07-08 20:05:10 +0300

    ribbon timeout

--- gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
+++ gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
@@ -42,11 +42,11 @@ public class GatewayApplication {
         return new Filter();
     }
 
-    private EurekaInstanceConfigBean eurekaInstanceConfig;
+
+//    private EurekaInstanceConfigBean eurekaInstanceConfig;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
-    @Primary
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
@@ -65,67 +65,67 @@ public class GatewayApplication {
     }
 
 
-    @Primary
-    @Autowired
-//    @Profile("docker")
-    public EurekaInstanceConfigBean DockerSwarm_EurekaClient(InetUtils inetUtils)
-    {
-
-
-        try{
-            eurekaInstanceConfig= new EurekaInstanceConfigBean(inetUtils);
-
-            final String HostName = System.getenv("HOSTNAME"); //container hostname 가져옴, container_id 일 것임.
-            logger.info("HOSTNAME : " + HostName);
-
-
-            Optional<NetworkInterface> net = Optional.of(NetworkInterface.getByName("eth2"));
-
-            logger.info("Network instance inetaddress: " + net.get().getInetAddresses());
-            logger.info("Network instance name: " + net.get().getName());
-
-            Enumeration<InetAddress> inetAddress = net.get().getInetAddresses();
-
-            InetAddress current = inetAddress.nextElement();
-
-            logger.info("Get Current Address : " + current.toString());
-
-            String address = current.toString().split("/")[1];
-
-/*
-			String address = null;
-
-			while(inetAddress.hasMoreElements())
-			{
-				current = inetAddress.nextElement();
-                logger.info("current address 1 : " + current.toString());
-				if(!current.isLoopbackAddress())
-				{
-					address = current.toString();
-                    logger.info("current address2: ", address);
-					break;
-				}
-			}
-*/
-            logger.info(" HostName : " + HostName);
-            logger.info(" Address : " + address);
-
-            eurekaInstanceConfig.setHostname(HostName);
-            eurekaInstanceConfig.setPreferIpAddress(true);
-            eurekaInstanceConfig.setIpAddress(address);
-            eurekaInstanceConfig.setNonSecurePort(9000);
-
-            logger.info("Eureka Config : "  + eurekaInstanceConfig.toString());
-
-            return eurekaInstanceConfig;
-
-
-        }catch(Exception e)
-        {
-            logger.info("EEEEEEEEEEEEEEERRRR");
-            return null;
-        }
-
-
-    }
+//    @Primary
+//    @Autowired
+////    @Profile("docker")
+//    public EurekaInstanceConfigBean DockerSwarm_EurekaClient(InetUtils inetUtils)
+//    {
+//
+//
+//        try{
+//            eurekaInstanceConfig= new EurekaInstanceConfigBean(inetUtils);
+//
+//            final String HostName = System.getenv("HOSTNAME"); //container hostname 가져옴, container_id 일 것임.
+//            logger.info("HOSTNAME : " + HostName);
+//
+//
+//            Optional<NetworkInterface> net = Optional.of(NetworkInterface.getByName("eth2"));
+//
+//            logger.info("Network instance inetaddress: " + net.get().getInetAddresses());
+//            logger.info("Network instance name: " + net.get().getName());
+//
+//            Enumeration<InetAddress> inetAddress = net.get().getInetAddresses();
+//
+//            InetAddress current = inetAddress.nextElement();
+//
+//            logger.info("Get Current Address : " + current.toString());
+//
+//            String address = current.toString().split("/")[1];
+//
+///*
+//			String address = null;
+//
+//			while(inetAddress.hasMoreElements())
+//			{
+//				current = inetAddress.nextElement();
+//                logger.info("current address 1 : " + current.toString());
+//				if(!current.isLoopbackAddress())
+//				{
+//					address = current.toString();
+//                    logger.info("current address2: ", address);
+//					break;
+//				}
+//			}
+//*/
+//            logger.info(" HostName : " + HostName);
+//            logger.info(" Address : " + address);
+//
+//            eurekaInstanceConfig.setHostname(HostName);
+//            eurekaInstanceConfig.setPreferIpAddress(true);
+//            eurekaInstanceConfig.setIpAddress(address);
+//            eurekaInstanceConfig.setNonSecurePort(9000);
+//
+//            logger.info("Eureka Config : "  + eurekaInstanceConfig.toString());
+//
+//            return eurekaInstanceConfig;
+//
+//
+//        }catch(Exception e)
+//        {
+//            logger.info("EEEEEEEEEEEEEEERRRR");
+//            return null;
+//        }
+//
+//
+//    }
 }
