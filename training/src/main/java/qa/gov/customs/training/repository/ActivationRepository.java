0b2f03b4644e44ca98a64a99f9c0b9b0b7a7d276
jaya <jaya@jad.cpga.net.qa>
2019-07-08 09:12:13 +0300

    manage activation

--- training/src/main/java/qa/gov/customs/training/repository/ActivationRepository.java
+++ training/src/main/java/qa/gov/customs/training/repository/ActivationRepository.java
@@ -27,4 +27,6 @@ public interface ActivationRepository extends JpaRepository<TacCourseActivation,
 //    @Query(value="Select a. from Tac_Course_Activation a where a.course_Id in (select b.course_id from Tac_Course_Master b where lower(b.course_Name) LIKE %:name% ) order by a.activation_Id desc",nativeQuery=true)
 //    List<TacCourseActivation> findAllByCourseNameContaining(String name);
 
+//    select * from ( select row_.*, rownum rownum_ from (Select * from Tac_Course_Activation where course_Id in (select course_id from Tac_Course_Master where lower(course_Name) LIKE %:name% ) order by activation_Id desc)row_ where rownum <= ?) where rownum_ > ?
+
 }
