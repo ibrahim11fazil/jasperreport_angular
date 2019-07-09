522cbfc22a7fe4967bba204cdd6103ba7f62a291
shahin <shahin@customs.gov.qa>
2019-07-09 09:31:45 +0300

    list mawared jobs

--- employee/src/main/java/qa/gov/customs/employee/service/MawaredService.java
+++ employee/src/main/java/qa/gov/customs/employee/service/MawaredService.java
@@ -3,6 +3,7 @@ package qa.gov.customs.employee.service;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import qa.gov.customs.employee.entity.MawaredMaster;
+import qa.gov.customs.employee.utils.models.MawaredJobs;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface MawaredService {
 
     List<MawaredMaster> findByLegacyCode(String jobCode);
     List<MawaredMaster> findByEmail(String email);
+    List<MawaredJobs> listJobs();
+    
 }
