d2cc15221ccacb29729abc40027ec99a36a5fcc0
sarath raj <sraj@customs.gov.qa>
2019-07-09 07:34:43 +0300

    build version 0.3.0

--- gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
+++ gateway/src/main/java/qa/gov/customs/gateway/GatewayApplication.java
@@ -36,14 +36,14 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public Filter filter()
-    {
-        return new Filter();
-    }
+//    @Bean
+//    public Filter filter()
+//    {
+//        return new Filter();
+//    }
 
 
-//    private EurekaInstanceConfigBean eurekaInstanceConfig;
+    private EurekaInstanceConfigBean eurekaInstanceConfig;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
@@ -65,67 +65,67 @@ public class GatewayApplication {
     }
 
 
-//    @Primary
-//    @Autowired
-////    @Profile("docker")
-//    public EurekaInstanceConfigBean DockerSwarm_EurekaClient(InetUtils inetUtils)
-//    {
-//
-//
-//        try{
-//            eurekaInstanceConfig= new EurekaInstanceConfigBean(inetUtils);
-//
-//            final String HostName = System.getenv("HOSTNAME"); //container hostname 가져옴, container_id 일 것임.
-//            logger.info("HOSTNAME : " + HostName);
-//
-//
-//            Optional<NetworkInterface> net = Optional.of(NetworkInterface.getByName("eth2"));
-//
-//            logger.info("Network instance inetaddress: " + net.get().getInetAddresses());
-//            logger.info("Network instance name: " + net.get().getName());
-//
-//            Enumeration<InetAddress> inetAddress = net.get().getInetAddresses();
-//
-//            InetAddress current = inetAddress.nextElement();
-//
-//            logger.info("Get Current Address : " + current.toString());
-//
-//            String address = current.toString().split("/")[1];
-//
-///*
-//			String address = null;
-//
-//			while(inetAddress.hasMoreElements())
-//			{
-//				current = inetAddress.nextElement();
-//                logger.info("current address 1 : " + current.toString());
-//				if(!current.isLoopbackAddress())
-//				{
-//					address = current.toString();
-//                    logger.info("current address2: ", address);
-//					break;
-//				}
-//			}
-//*/
-//            logger.info(" HostName : " + HostName);
-//            logger.info(" Address : " + address);
-//
-//            eurekaInstanceConfig.setHostname(HostName);
-//            eurekaInstanceConfig.setPreferIpAddress(true);
-//            eurekaInstanceConfig.setIpAddress(address);
-//            eurekaInstanceConfig.setNonSecurePort(9000);
-//
-//            logger.info("Eureka Config : "  + eurekaInstanceConfig.toString());
-//
-//            return eurekaInstanceConfig;
-//
-//
-//        }catch(Exception e)
-//        {
-//            logger.info("EEEEEEEEEEEEEEERRRR");
-//            return null;
-//        }
-//
-//
-//    }
+    @Primary
+    @Autowired
+//    @Profile("docker")
+    public EurekaInstanceConfigBean DockerSwarm_EurekaClient(InetUtils inetUtils)
+    {
+
+
+        try{
+            eurekaInstanceConfig= new EurekaInstanceConfigBean(inetUtils);
+
+            final String HostName = System.getenv("HOSTNAME"); //container hostname x, container_id y
+            logger.info("HOSTNAME : " + HostName);
+
+
+            Optional<NetworkInterface> net = Optional.of(NetworkInterface.getByName("mybridge"));
+
+            logger.info("Network instance inetaddress: " + net.get().getInetAddresses());
+            logger.info("Network instance name: " + net.get().getName());
+
+            Enumeration<InetAddress> inetAddress = net.get().getInetAddresses();
+
+            InetAddress current = inetAddress.nextElement();
+
+            logger.info("Get Current Address : " + current.toString());
+
+            String address = current.toString().split("/")[1];
+
+/*
+			String address = null;
+
+			while(inetAddress.hasMoreElements())
+			{
+				current = inetAddress.nextElement();
+                logger.info("current address 1 : " + current.toString());
+				if(!current.isLoopbackAddress())
+				{
+					address = current.toString();
+                    logger.info("current address2: ", address);
+					break;
+				}
+			}
+*/
+            logger.info(" HostName : " + HostName);
+            logger.info(" Address : " + address);
+
+            eurekaInstanceConfig.setHostname(HostName);
+            eurekaInstanceConfig.setPreferIpAddress(true);
+            eurekaInstanceConfig.setIpAddress(address);
+            eurekaInstanceConfig.setNonSecurePort(9000);
+
+            logger.info("Eureka Config : "  + eurekaInstanceConfig.toString());
+
+            return eurekaInstanceConfig;
+
+
+        }catch(Exception e)
+        {
+            logger.info("EEEEEEEEEEEEEEERRRR");
+            return null;
+        }
+
+
+    }
 }
