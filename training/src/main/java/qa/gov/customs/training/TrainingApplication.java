290cd4abf477bdd1428eb2a7bd206103c26c2a33
sarath raj <sraj@customs.gov.qa>
2019-07-10 21:07:29 +0300

    build version 0.4.9

--- training/src/main/java/qa/gov/customs/training/TrainingApplication.java
+++ training/src/main/java/qa/gov/customs/training/TrainingApplication.java
@@ -4,12 +4,14 @@ package qa.gov.customs.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class TrainingApplication {
 
     public static void main(String[] args) {
