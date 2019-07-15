0a883c1632ba262245aa8cc990db202b926e8297
sarath raj <sraj@customs.gov.qa>
2019-07-15 13:14:16 +0300

    employee model added

--- ui/src/app/models/job-card-data.ts
+++ ui/src/app/models/job-card-data.ts
@@ -1,2 +1,47 @@
-export class JobCardData {
+
+export interface TacJobcardCondition {
+    jobConditions: string;
+    conditionsId: number;
+}
+
+export interface TacJobcardSkill {
+    jobSkills: string;
+    skillsID: number;
+}
+
+export interface TacJobcardDuty {
+    dutyDescription: string;
+    dutiesId: number;
+}
+
+export interface JobCardData {
+    job: string;
+    jobGrade: string;
+    jobGroup: string;
+    jobTitle: string;
+    specialGroup: string;
+    jobcardNo: number;
+    tacJobcardConditions: TacJobcardCondition[];
+    tacJobcardSkills: TacJobcardSkill[];
+    tacJobcardDuties: TacJobcardDuty[];
+}
+
+export interface IJobCardDataResponse {
+    status: Boolean;
+    code:number;
+    message:String;
+    data: JobCardData;
+}
+
+export class JobCardDataSearch {
+    job?:String
+    start:Number
+	limit:Number
+}
+
+export interface IJobCardDataSearchResponse {
+    status: Boolean;
+    code:number;
+    message:String;
+    data: JobCardData[];
 }
