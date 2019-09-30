6bef46b24143b7349f605af226c2a3a953bb9f63
sarath raj <sraj@customs.gov.qa>
2019-09-30 14:26:23 +0300

    permissions updated

--- user/src/main/java/qa/gov/custom/user/repository/RoleRepository.java
+++ user/src/main/java/qa/gov/custom/user/repository/RoleRepository.java
@@ -1,8 +1,11 @@
 package qa.gov.custom.user.repository;
 
+import org.hibernate.annotations.SQLInsert;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import qa.gov.custom.user.entity.Role;
 import qa.gov.custom.user.entity.UserMaster;
@@ -17,10 +20,21 @@ import java.util.List;
 @Transactional
 public interface RoleRepository extends JpaRepository<Role, BigInteger> {
 
-    @Query(value="select ID,NAME,REMARK from ROLE ",nativeQuery=true)
+    @Query(value="select ID,NAME from ROLE ",nativeQuery=true)
     List<Object[]> findallAllRolesWithIDAndName();
 
     @Query(value="select ID,PERMISSION_ID,ROLE_ID,REMARK from PERMISSION_ROLE where ROLE_ID=:roleId ",nativeQuery=true)
     List<Object[]> findAllPermissionForRole(BigInteger roleId);
+
+
+    @Modifying
+    @Query(value="delete from PERMISSION_ROLE where ROLE_ID=:roleId",nativeQuery=true)
+    void deletePermissionRole(Long roleId);
+
+
+    @Modifying
+    @Transactional
+    @Query(value="insert into PERMISSION_ROLE(PERMISSION_ID,ROLE_ID) values (:permissionId,:roleId)",nativeQuery=true)
+    void updateRoleAndPermission(@Param("permissionId") BigInteger permissionId ,@Param("roleId")  BigInteger roleId);
 
 }
