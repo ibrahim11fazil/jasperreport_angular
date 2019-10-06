b42463ade01e14ca9ddac0408e23a3ab45925c43
sarath raj <sraj@customs.gov.qa>
2019-10-06 12:51:14 +0300

    updated workflow request

---
+++ workflow-camuda/src/main/java/qa/gov/customs/workflowcamuda/proxy/UserSSOProxy.java
@@ -0,0 +1,23 @@
+package qa.gov.customs.workflowcamuda.proxy;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import qa.gov.customs.workflowcamuda.model.ResponseType;
+import java.math.BigInteger;
+
+@FeignClient(name="user")
+public interface UserSSOProxy {
+
+    @PostMapping ("/find-all-system-users-by-role-role-id-workflow/{roleId}/{token}")
+    ResponseType findAllSystemUsersByRole(@PathVariable("roleId") BigInteger roleId, @PathVariable("token") String token);
+
+    @PostMapping("/check-the-user-is-head-of-training-workflow/{id}/{wtoken}")
+    ResponseType checkUserIsHeadOfTraining(@PathVariable("id") String id,@PathVariable("wtoken") String wtoken);
+
+    @PostMapping(value="/get-head-of-training-and-continuing-edu-workflow/{wtoken}",consumes= MediaType.APPLICATION_JSON_VALUE)
+    ResponseType getTrainingDepartmentHeads(@PathVariable("wtoken") String wtoken);
+
+}
