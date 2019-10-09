b9c470048a3f957526d41bcec6e4d5b826a720c0
jaya <jaya@jad.cpga.net.qa>
2019-10-09 10:47:40 +0300

    dashboard

--- ui/src/app/training/welcome/welcome.component.ts
+++ ui/src/app/training/welcome/welcome.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TrainingService } from 'app/service/training/training.service';
+import { ToastrService } from 'ngx-toastr';
+import { SystemUserService } from 'app/service/user/system-user.service';
+import { ActivatedRoute } from '@angular/router';
+import { PageTitleService } from 'app/core/page-title/page-title.service';
+
 
 @Component({
   selector: 'ms-welcome',
@@ -7,9 +15,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private modal: NgbModal,
+    private trainingService: TrainingService,
+    private toastr: ToastrService,
+    private userService: SystemUserService,
+
+    private activatedRoute: ActivatedRoute,
+    private pageTitleService: PageTitleService) { }
 
   ngOnInit() {
   }
+  statsCard: any[] = [
 
+    {
+      card_color: "accent-bg",
+      title: "Attended Courses",
+      // number : "1,425",
+      icon: "assessment"
+    },
+    {
+      card_color: "success-bg",
+      title: " Ongoing Courses",
+      //number : "6,101",
+      icon: "assignment_return",
+    },
+    {
+      card_color: "warn-bg",
+      title: "Request Future Courses",
+      //number : "5,218",
+      icon: "new_releases"
+    },
+    {
+      card_color: "course-bg",
+      title: " Approved Courses",
+      //number : "6,101",
+      icon: "assignment",
+    },
+    {
+      card_color : "primary-bg",
+    title : "Smart Engine Suggestion",
+    icon : "remove_red_eye",
+  },
+  {
+    card_color : "brown-bg",
+  title : "My Tasks",
+  icon : "add_alert",
+}
+  ]
 }
