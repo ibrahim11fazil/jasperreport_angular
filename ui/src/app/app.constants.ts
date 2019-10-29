afb5d6ad2d7d11377521b2f8289b0288f1d1d6eb
sarath raj <sraj@customs.gov.qa>
2019-10-29 09:12:42 +0300

    build version 0.8.1

--- ui/src/app/app.constants.ts
+++ ui/src/app/app.constants.ts
@@ -101,7 +101,7 @@ export const	GENERAL_COURSE =4
 
 //export const BASE_URL ="http://localhost:9000"
 
-export const VERSION_UI ="qa_0.7.8"
+export const VERSION_UI ="qa_0.8.1"
 
 //Disable if gateway is down -- for development
 var gateway = true
