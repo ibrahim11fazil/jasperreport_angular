5b232f2f67e174602efbdb44d5b35bc2b847c048
jaya <jaya@jad.cpga.net.qa>
2019-11-05 10:44:55 +0300

    number validation activation page

--- ui/src/app/training/activate-course/activate-course.component.ts
+++ ui/src/app/training/activate-course/activate-course.component.ts
@@ -518,6 +518,13 @@ export class ActivateCourseComponent implements OnInit {
     )
     
   }
+
+
+  numericOnly(event): boolean {    
+    let patt = /^([0-9])$/;
+    let result = patt.test(event.key);
+    return result;
+}
  
 
 }
