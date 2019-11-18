baa3b471e2aee4be393f7b56654849ffa5a0c8bb
jaya <jaya@Jad.local>
2019-11-18 14:08:06 +0300

    build version 0.10.6

--- buildJavaAndUI.sh
+++ buildJavaAndUI.sh
@@ -39,12 +39,12 @@ mvn clean install -f cis
 [ $? -eq 0 ] || exit 1
 
 #npm install -g @angular/cli
-#[ $? -eq 0 ] || exit 1
+#[ qa? -eq 0 ] || exit 1
 
 #npm install --prefix ./ui
-#[ $? -eq 0 ] || exit 1
+#[ qa? -eq 0 ] || exit 1
 
 pushd ui
 node --max_old_space_size=8192  node_modules/@angular/cli/bin/ng build --prod
-[ $? -eq 0 ] || exit 1
+[ qa? -eq 0 ] || exit 1
 popd
