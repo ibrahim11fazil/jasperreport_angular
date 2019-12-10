34df155061a3a1e8db91abb50d7eda5fb45f6a55
jaya <jaya@jad.cpga.net.qa>
2019-12-10 07:39:19 +0300

    build version 0.10.8

--- ui/src/app/app.constants.ts
+++ ui/src/app/app.constants.ts
@@ -107,7 +107,7 @@ export const	GENERAL_COURSE =4
 
 //export const BASE_URL ="http://localhost:9000"
 
-export const VERSION_UI ="qa_0.10.7"
+export const VERSION_UI ="qa_0.10.8"
 
 //Disable if gateway is down -- for development
 var gateway = true
